042c6747d3e237b84ba8bb74f4c1a80c99fe9ab6
MR-Nordsee <46679815+MR-Nordsee@users.noreply.github.com>
2026-04-06 11:06:24 +0200

    change replace technic to full line replace

--- init.sh
+++ init.sh
@@ -19,9 +19,9 @@ if [ -n "$DB_BACKUP_CRONJOB" ]; then
         echo "Valid cron timing found: $DB_BACKUP_CRONJOB"
         # Replace the timing part of the line that contains the command
         if [[ "$OSTYPE" == "darwin"* ]]; then # For testing check for darwin OS
-            sed -i '' -E "s|^[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+$CRON_COMMAND|$DB_BACKUP_CRONJOB $CRON_COMMAND|" "$CRON_FILE"
+            sed -i '' "s|^.* $CRON_COMMAND|$DB_BACKUP_CRONJOB $CRON_COMMAND|" "$CRON_FILE"
         else
-            sed -i -E "s|^[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+[0-9\*/,-]+[[:space:]]+$CRON_COMMAND|$DB_BACKUP_CRONJOB $CRON_COMMAND|" "$CRON_FILE"
+            sed -i "s|^.* $CRON_COMMAND|$DB_BACKUP_CRONJOB $CRON_COMMAND|" "$CRON_FILE"
         fi
         echo "Cronjob timing updated in $CRON_FILE"
     else
